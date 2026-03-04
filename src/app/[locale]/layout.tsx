17a584b56159aa606156faad039fa49ffad524d9
Kakajan-12 <83004880+Kakajan-12@users.noreply.github.com>
2026-03-04 06:15:29 +0500

    fix next-inlt

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -43,7 +43,7 @@ export default async function RootLayout({
         notFound();
     }
 
-    const messages = await getMessages();
+    const messages = await getMessages({ locale });
 
     return (
         <html lang={locale}>
--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -2,14 +2,13 @@ import {getRequestConfig} from 'next-intl/server';
 import {hasLocale} from 'next-intl';
 import {routing} from './routing';
 
-export default getRequestConfig(async ({requestLocale}) => {
-    const requested = await requestLocale;
-    const locale = hasLocale(routing.locales, requested)
-        ? requested
+export default getRequestConfig(async ({ locale }) => {
+    const validLocale = hasLocale(routing.locales, locale)
+        ? locale
         : routing.defaultLocale;
 
     return {
-        locale,
-        messages: (await import(`../../messages/${locale}.json`)).default
+        locale: validLocale,
+        messages: (await import(`../../messages/${validLocale}.json`)).default
     };
 });
